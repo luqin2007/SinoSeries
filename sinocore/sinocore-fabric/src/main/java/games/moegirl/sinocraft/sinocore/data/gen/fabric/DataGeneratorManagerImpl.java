1851fbd8ea11596b3b1c2746b223eabd3191af63
luqin2007 <lq2007lq@hotmail.com>
2025-03-24 00:15:38 +0800

    fix: fabric 下 runclient 报错

--- sinocore/sinocore-fabric/src/main/java/games/moegirl/sinocraft/sinocore/data/gen/fabric/DataGeneratorManagerImpl.java
+++ sinocore/sinocore-fabric/src/main/java/games/moegirl/sinocraft/sinocore/data/gen/fabric/DataGeneratorManagerImpl.java
@@ -1,10 +1,21 @@
 package games.moegirl.sinocraft.sinocore.data.gen.fabric;
 
+import games.moegirl.sinocraft.sinocore.data.gen.IDataGenContext;
 import games.moegirl.sinocraft.sinocore.data.gen.IDataGenerator;
+import net.minecraft.data.DataProvider;
 import org.apache.commons.lang3.NotImplementedException;
+
+import java.util.function.Function;
+import java.util.function.Supplier;
 
 public class DataGeneratorManagerImpl {
     public static IDataGenerator _createDataProvider(String modId) {
-        throw new NotImplementedException("Fabric data generator is not supported now!");
+//        throw new NotImplementedException("Fabric data generator is not supported now!");
+        return new IDataGenerator() {
+            @Override
+            public <T extends DataProvider> Supplier<T> put(Function<IDataGenContext, ? extends T> builder, boolean run) {
+                return () -> null;
+            }
+        };
     }
 }
